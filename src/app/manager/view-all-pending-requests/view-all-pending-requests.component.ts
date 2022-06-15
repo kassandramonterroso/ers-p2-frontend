78964317980e2ed65350bfae87db20bfdec5f5ce
kassandra <kassandramntrrs@gmail.com>
2022-06-15 16:16:26 -0400

    d

--- src/app/manager/view-all-pending-requests/view-all-pending-requests.component.ts
+++ src/app/manager/view-all-pending-requests/view-all-pending-requests.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-view-all-pending-requests',
+  selector: 'view-all-pending-requests',
   templateUrl: './view-all-pending-requests.component.html',
   styleUrls: ['./view-all-pending-requests.component.css']
 })
