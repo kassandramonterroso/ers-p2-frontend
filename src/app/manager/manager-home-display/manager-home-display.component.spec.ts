c692f2a9b25e10eef9c06aec9d5ed63e06cb16fb
kassandramonterroso <104796916+kassandramonterroso@users.noreply.github.com>
2022-06-20 16:19:46 -0400

    Update manager-home-display.component.spec.ts

--- src/app/manager/manager-home-display/manager-home-display.component.spec.ts
+++ src/app/manager/manager-home-display/manager-home-display.component.spec.ts
@@ -20,4 +20,11 @@ describe('ManagerHomeDisplayComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+   it(`should render title`, () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('title')?.textContent).toContain('Manager Home');
+  });
+  
 });
