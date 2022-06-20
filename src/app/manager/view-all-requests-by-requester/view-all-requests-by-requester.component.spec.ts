ea9c0a060306dc7a540c209b2912e80a5ddf0193
kassandramonterroso <104796916+kassandramonterroso@users.noreply.github.com>
2022-06-20 16:16:44 -0400

    Update view-all-requests-by-requester.component.spec.ts

--- src/app/manager/view-all-requests-by-requester/view-all-requests-by-requester.component.spec.ts
+++ src/app/manager/view-all-requests-by-requester/view-all-requests-by-requester.component.spec.ts
@@ -20,4 +20,16 @@ describe('ViewAllRequestsByRequesterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it(`should call ngOnInit`, function(){
+    const vARBR = fixture.componentInstance;
+    expect(vARBR.ngOnInit).toHaveBeenCalled();
+  });
+
+  it(`should render title`, () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h2')?.textContent).toContain('View All Requests By Employee');
+  });
+  
 });
